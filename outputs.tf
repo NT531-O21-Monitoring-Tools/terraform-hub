7523cd273c033c6f1d3499cb82c25adc36ff48ce
Luu Minh Thien <128106955+th1enlm02@users.noreply.github.com>
2024-11-06 21:54:59 +0700

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,12 @@
-# output "instance" {
-#   value = {
-#     vpn_instance = aws_instance.vpn_instance[*].id
-#     monitoring_frontend_instance = aws_instance.monitoring_fe_instance[*].id
-#     monitoring_backend_instance = aws_instance.monitoring_be_instance[*].id
-#     app_instance = aws_instance.k3s_instance[*].id
-#   }
-# }
+output "instance_ips" {
+    value = {
+        bastion_instance = aws_instance.bastion_instance[*].private_ip
+        monitoring_frontend_instance = aws_instance.monitoring_fe_instance[*].private_ip
+        monitoring_backend_instance = aws_instance.monitoring_be_instance[*].private_ip
+        cluster_instance = aws_instance.k3s_instance[*].private_ip
+    }
+}
 
-# output "instance_ips" {
-#   value = {
-#     vpn_instance = aws_instance.vpn_instance[*].private_ip
-#     monitoring_frontend_instance = aws_instance.monitoring_fe_instance[*].private_ip
-#     monitoring_backend_instance = aws_instance.monitoring_be_instance[*].private_ip
-#     app_instance = aws_instance.k3s_instance[*].private_ip
-#   }
-# }
-
-# output "vpn_public_ip" {
-#   value = aws_instance.vpn_instance[*].public_ip
-# }
+output "bastion_instance_ip_public" {
+    value = aws_instance.bastion_instance[*].public_ip
+}
